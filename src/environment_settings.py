c8b89a6c62c9420ea3f6e6697ea87ec7dee3a308
Jacob Ogden <jacob.ogden@yahoo.com>
2025-10-15 09:29:40 -0400

    Added a var to allow forcing use of the production environment during debug

--- src/environment_settings.py
+++ src/environment_settings.py
@@ -19,10 +19,12 @@ os.environ.setdefault("PYDANTIC_ERRORS_INCLUDE_URL", "false")
 
 CWD = Path(__file__).parent if getattr(sys, "frozen", False) else Path.cwd()
 
+testing = False
+
 
 class Settings(BaseSettings):
   model_config = SettingsConfigDict(
-    env_file=CWD / ("testing.env" if __debug__ else "production.env"),
+    env_file=CWD / ("testing.env" if __debug__ and testing else "production.env"),
     env_file_encoding="utf-8",
     env_ignore_empty=True,
   )
